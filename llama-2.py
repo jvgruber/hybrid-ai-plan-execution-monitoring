7a04c5a3f0694722b3b51f8ce11ab12572417b5f
jvgruber <julian.v.gruber@gmail.com>
2024-06-21 16:11:36 +0200

    init

---
+++ llama-2.py
@@ -0,0 +1,20 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+
+access_token = "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"
+model = "meta-llama/Llama-2-7b-chat-hf"
+
+tokenizer = AutoTokenizer.from_pretrained(model, token=access_token)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model, 
+    token=access_token
+)
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
