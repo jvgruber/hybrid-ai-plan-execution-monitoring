f8fd801c2faf347ba229260f49dc6ac14849ac4f
Nikolaus Lajtai <nikolaus.lajtai@gmx.at>
2024-06-22 16:24:06 +0200

    change agent to robot

--- asp_constants.py
+++ asp_constants.py
@@ -1,7 +1,7 @@
 INTERPRETER_RULES = {
-    'move': lambda x: f"The agent moves to location {x} from the previous location.",
-    'pickup': lambda x: f"The agent picks up package {x} at the current location.",
-    'release': lambda x: f"The agent releases package {x} at the current location."
+    'move': lambda x: f"The robot moves to location {x} from the previous location.",
+    'pickup': lambda x: f"The robot picks up package {x} at the current location.",
+    'release': lambda x: f"The robot releases package {x} at the current location."
 }
 
 TEST_PLAN = [['move(B)', 'pickup(1)', 'move(C)', 'move(E)', 'release(1)', 'pickup(2)', 'move(C)', 'move(B)', 'release(2)']]
