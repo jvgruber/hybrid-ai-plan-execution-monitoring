602ddc884080dc6c3bb9059804024f4e811b4d95
jvgruber <julian.v.gruber@gmail.com>
2024-06-22 10:36:53 +0200

    hf-inference-api

--- llama-2.py
+++ llama-2.py
@@ -1,20 +1,18 @@
-import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import transformers
-
-access_token = "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"
-model = "meta-llama/Llama-2-7b-chat-hf"
-
-tokenizer = AutoTokenizer.from_pretrained(model, token=access_token)
-
-model = AutoModelForCausalLM.from_pretrained(
-    model, 
-    token=access_token
-)
-
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model,
-    torch_dtype=torch.float16,
-    device_map="auto",
-)
+import requests
+API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-2-70b-chat-hf"
+API_TOKEN = "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"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    # try:
+    #     response.raise_for_status()  # Check if the request was successful
+    return response.json()  # Parse JSON response
+    # except requests.exceptions.HTTPError as http_err:
+    #     print(f"HTTP error occurred: {http_err}")  # Print HTTP error
+    # except requests.exceptions.RequestException as req_err:
+    #     print(f"Request error occurred: {req_err}")  # Print request error
+    # except ValueError:
+    #     print(f"Invalid JSON received: {response.text}")  # Print response content if JSON parsing fails
+    # return None
+data = query("Can you please let us know more details about your ")
+print(data)
