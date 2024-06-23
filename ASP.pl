94307bca9a997e866f085808ee264f8358a2a481
jvgruber <julian.v.gruber@gmail.com>
2024-06-23 19:40:51 +0200

    asp wip

--- ASP.pl
+++ ASP.pl
@@ -10,26 +10,30 @@
 % output should be an explaination to the observations
 
 % Define time steps
-time(0..10).
+time(0..5).
 
 % INIT
 at(0,a).
-ploc(0, one, b).
-ploc(0, two, e).
+ploc(0, one, a).
+carry(0, empty).
+% ploc(0, two, e).
 
 % GOAL
-ploc(10, one, e).
-ploc(10, two, b).
+ploc(5, one, b).
+at(5, a).
+carry(2, empty).
 
 % Define locations
 location(a; b; c).
 
 % Define packets
-packet(none; one; two; three).
+packet(empty; one;).
 
 % Define actions
 action(move(X)) :- location(X).
 action(load(X)) :- packet(X).
+action(release(X)) :- packet(X).
+action(wait).
 
 % Not more than one execution at a time.
 1 { execute(T, A) : action(A) } 1 :- time(T).
@@ -40,9 +44,9 @@ action(load(X)) :- packet(X).
 :- at(T, A), at(T, B), A!=B.
 
 % Definition: packet at location
-1 { ploc(T, A, X) : location(X) } 1 :- time(T), packet(A), A!=none.
-:- ploc(T, A, X), ploc(T, A, Y), X!=Y, A!=none.
-ploc(T, none, X) :- time(T), location(X).
+1 { ploc(T, A, X) : location(X) } 1 :- time(T), packet(A), A!=empty.
+:- ploc(T, A, X), ploc(T, A, Y), X!=Y, A!=empty.
+ploc(T, empty, X) :- time(T), location(X).
 
 % Definition: execute move action
 execute(T, move(B)) :- time(T), location(A), location(B), at(T, A), at(T+1, B), A!=B.
@@ -53,20 +57,22 @@ A!=B :- time(T), location(A), location(B), at(T, A), execute(T, move(B)).
 1 { carry(T, A) : packet(A) } 1 :- time(T).
 :- carry(T, A), carry(T, B), A!=B.
 ploc(T, A, X) :- carry(T, A), at(T, X).
+% ploc(T+1, A, X) :- ploc(T, A, X), not carry(T, X).
+% ploc(T+1, A, X) :- ploc(T, A, X), carry(T, X), location(B), not execute(T, move(B)).
 
 % Definition: execute load
-execute(T, load(A)) :- time(T), packet(A), carry(T, none), carry(T+1, A).
-carry(T, none) :- execute(T, load(A)).
+execute(T, load(A)) :- time(T), packet(A), carry(T, empty), carry(T+1, A).
+carry(T, empty) :- execute(T, load(A)).
 carry(T+1, A) :- execute(T, load(A)).
-:- load(none).
+:- execute(T, load(emtpy)).
 ploc(T, A, X) :- location(X), execute(T, load(A)), at(T, X).
 at(T, X) :- location(X), execute(T, load(A)), ploc(T, A, X).
 
 % Definition: execute release
-execute(T, release(A)) :- time(T), packet(A), carry(T+1, none), carry(T, A).
+execute(T, release(A)) :- time(T), packet(A), carry(T+1, empty), carry(T, A).
 carry(T, A) :- execute(T, release(A)).
-carry(T+1, none) :- execute(T, release(A)).
-:- release(none).
+carry(T+1, empty) :- execute(T, release(A)).
+:- execute(T, release(emtpy)).
 
 
 % Minimize faults
